c225639d8e71592c1b4254d4fcb39ab6148945fc
西航太 <>
2021-08-21 15:51:05 +0900

    create react app

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Screen from "./Screen/Screen";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Screen />
+      </header>
+    </div>
+  );
+}
+
+export default App;
